149b5382f1a91287dfa4085108356d6ab97b41c7
flizzyflo <vip3r1@gmx.de>
2024-12-17 19:03:07 +0100

    initial creation

---
+++ main.py
@@ -0,0 +1,7 @@
+from scaper import extract_content
+
+if __name__ == '__main__':
+    content_rows = extract_content()
+    print(content_rows)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
