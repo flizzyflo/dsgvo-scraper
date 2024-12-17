e16b639b51864aca2f70e3833cc27379423369cc
flizzyflo <vip3r1@gmx.de>
2024-12-17 19:17:12 +0100

    created csv generator for dictionary content

---
+++ utils/csv_generator.py
@@ -0,0 +1,20 @@
+import os.path
+from pathlib import Path
+from typing import Union, List, Dict
+import pandas as pd
+
+class NoDirException(Exception):
+    """Individual exception for non dir paths"""
+    pass
+
+def generate_csv(content: List[Dict[str, str]], path: Union[str|Path]):
+
+    if isinstance(path, str):
+        path = Path(str)
+
+    if not path.is_dir():
+        raise NoDirException(path, " is not directory. Can not store file to that path. Please provide directory.")
+
+    df = pd.DataFrame(content)
+
+    df.to_csv(os.path.join(f"{path}{os.sep}extracted_dsgvo_content.csv"), index=False)
