72d357be2bc0e567414a4e2ff473352de6891c45
flizzyflo <vip3r1@gmx.de>
2024-12-20 20:59:32 +0100

    added constants for slicing

--- utils/transformer.py
+++ utils/transformer.py
@@ -5,11 +5,14 @@ from selenium.webdriver.remote.webelement import WebElement
 
 def extract_row_content(rows_per_page: List[WebElement], transformed_elements: List[Dict[str, str]]):
 
+    LENGTH_SUFFIX: int = 8
+    LENGTH_DATE: int = 10
+    START_FEE: int = 11
     try:
         for row in rows_per_page[1:]: # skip headline, does not contain relevant data
             date_fee_mixed, company, country, reason = row.text.split("\n")
-            reason = reason[:len(reason) - 8] # extract '»Details' from end
-            date, fee = date_fee_mixed[:10], date_fee_mixed[11:]
+            reason = reason[:len(reason) - LENGTH_SUFFIX] # remove '»Details' from end
+            date, fee = date_fee_mixed[:LENGTH_DATE], date_fee_mixed[START_FEE:]
 
             row_content = {"date": date,
                            "fee": fee,
